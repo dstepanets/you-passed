8a13cf5b05006bf9f9ad4e747024105a92348bf5
dstepanets <dm.stepanets@gmail.com>
2020-03-05 14:34:30 +0200

    Basic security configured. Working on web templates

---
+++ src/main/java/com/youpassed/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.youpassed.config;
+
+import com.youpassed.domain.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/admin").hasRole(User.Role.ADMIN.toString())
+				.antMatchers("/student").hasRole(User.Role.STUDENT.toString())
+				.antMatchers("/", "/register", "/static/**").permitAll()
+//				.anyRequest().authenticated()
+				.and()
+			.formLogin().loginPage("/login").permitAll()
+				.and()
+			.logout().permitAll();
+	}
+
+
+}
---
+++ src/main/java/com/youpassed/controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package com.youpassed.controller;
+
+import com.youpassed.domain.User;
+import com.youpassed.exception.ValidationException;
+import com.youpassed.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class AuthenticationController {
+
+	private final UserService userService;
+
+	@GetMapping({"/", ""})
+	public String index(){
+		return "index";
+	}
+
+	@GetMapping(value = {"/register"})
+	public String register(Model model) {
+		model.addAttribute("user", User.builder().build());
+		return "register";
+	}
+
+	@PostMapping(value = {"/register"})
+	public String register(@ModelAttribute @Valid User user) throws ValidationException {
+		userService.register(user);
+		return "login";
+	}
+
+}
